1c9858c4b3670f04900700ff9e3307e98ef844e6
ChayaTejaswini <118282140+ChayaTejaswini@users.noreply.github.com>
2022-11-17 12:17:25 +0530

    Add files via upload

---
+++ TestCodingAssesment/README.md
@@ -0,0 +1,23 @@
+# To run this Code :
+Platform - Windows 7,windows 8,windows 10
+
+# Minimum configuration :-
+Processor - AMD PRO A4-3350B APU
+System Type :- 64-bit Operating system
+
+
+
+
+#Procedures:-
+Step 1------>Enter into the project "TestvagrantCodingAssessment".
+Step 2------>Click on src/test/java.
+Step 3------>Click on the package of org.testvagrant.
+Step 4------>Enter into the class called "ImdbAndWikiPagesValidation.java"
+Step 5------>Run the Code.
+
+
+
+#Additional Details:-
+In src/main/java i have created the packages for genericUtilities and Objectrepositories.
+This is the Screenshot of Validation Process----->!validatation ScreenShotpath(Screenshot 2022-11-15 190418.png)
+
---
+++ TestCodingAssesment/src/test/java/org/testvagrant/ImdbAndWikiPagesValidation.java
@@ -0,0 +1,45 @@
+package org.testvagrant;
+
+import java.util.Arrays;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testvagrant.genericUtility.BaseClass;
+import org.testvagrant.objectrepositories.IMDBHomePage;
+import org.testvagrant.objectrepositories.Wikipedia;
+
+public class ImdbAndWikiPagesValidation extends BaseClass
+{
+	    @Test
+		public void ValidationOfImdbAndWikiPage()
+		{
+			IMDBHomePage id = new IMDBHomePage(driver);
+			driver.get("https://www.imdb.com/");
+			id.idmpMovieSearch();
+			String movieDate = id.movieReleaseDate();
+			String movieCountry = id.movieOrgin();
+			movieDate=movieDate.substring(0,18);
+			movieDate=movieDate.replaceAll(",", "");
+			movieDate=movieDate.replaceAll(" ", "");
+			
+			Wikipedia wik=new Wikipedia(driver);
+			driver.get("https://en.wikipedia.org/wiki/Wikipedia");
+			wik.wikiMovieSearch();
+			String movieDate2 = wik.wikiMovieReleaseDate();
+			movieDate2=movieDate2.replaceAll(" ", "");
+			String movieCountry2 = wik.wikiMovieOrgin();
+			//System.out.println(movieDate2);
+			
+			//Validation
+			char[] ch1=movieDate.toCharArray();
+			char[] ch2=movieDate2.toCharArray();
+			Arrays.sort(ch1);
+			Arrays.sort(ch2);
+			if(Arrays.equals(ch1, ch2))
+			{
+			System.out.println("Release Date is matching");
+			}
+			Assert.assertEquals(movieCountry, movieCountry2);
+		}
+}
+
